c5456cc3b2e6660c29dc1676dad748ba8742430c
Melomast <shermannut@gmail.com>
2024-02-12 20:58:03 +0600

    First hello world commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+/*
+func main() {
+	//Declaration
+	var name string
+	var a bool = true
+	var b int = 5
+	var c float32 = 3.14
+	var d string = "Abcdefg"
+
+	//Initialization
+	name = "German"
+
+	name2 := "Mary"
+
+	var name3 string = "Teyana"
+
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(name3)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
+*/
+
+/*
+func main() {
+	const pi = 3.14
+
+	fmt.Println(pi)
+}
+*/
+
+/*
+func main() {
+	// +
+	// -
+	// *
+	// /
+	// %
+
+	a := 10
+	b := 33
+
+	result := b % a
+	fmt.Println(result)
+}
+*/
